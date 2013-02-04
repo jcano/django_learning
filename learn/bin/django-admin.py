562ca906aebbdb7fee05bf927b570670b3c16ce4
TheSen <thesen@hefesto.(none)>
2013-02-04 23:43:07 +0000

    Initial commit. Project structure and first tutorial app

---
+++ learn/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/thesen/workspace/Projects/django/django_learning/learn/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
